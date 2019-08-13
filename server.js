920adfdb470f445a7cb423b6c7ab11db1f08107b
TJ park <sincerity0113@gmail.com>
2019-08-12 22:11:40 -0400

    not complete yet

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
